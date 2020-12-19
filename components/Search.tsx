fca21a21eeb7b30998eedeb59149ac4e3f2a990a
Macaulay Scott <macaulay.scott@averment.digital>
2020-12-19 13:12:52 +0000

    ..

---
+++ components/Search.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'search.css';
+
+const SearchFunction = (props) => {
+    return (
+        <div>
+            <p>this is a component</p>
+        </div>
+    );
+};
+
+export default SearchFunction;
+
